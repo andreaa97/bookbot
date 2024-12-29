3a52e5f03588ec91e9138ce1b172001f1ef69fcd
andreaa97 <andrea.abatantuono@outlook.it>
2024-12-29 10:50:50 +0100

    aggiunta funzione conta caratteri

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import string
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
 words = file_contents.split()
+#print(len(words))
+low_words = []
+letters = {}
+for word in words:
+    low = ""
+    low = word.lower()
+    low_words.append(low)
 
-print(words.__len__())
+
+for letter in string.printable:
+    letters[letter] = 0
+    
+for word in low_words:
+    for c in word:
+        letters[c] += 1
+
+print(letters)
