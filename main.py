bea4a0d7e9f04c70f3ade577c4d5ef5f0a33103b
andreaa97 <andrea.abatantuono@outlook.it>
2024-12-29 11:43:16 +0100

    creato rapporto n parole e occorrenza lettere

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import string
 
-with open("books/frankenstein.txt") as f:
+path_file = "books/frankenstein.txt"
+low_words = []
+letters = {}
+count_letters = []
+n_words = 0
+with open(path_file) as f:
     file_contents = f.read()
 
 words = file_contents.split()
-#print(len(words))
-low_words = []
-letters = {}
+n_words = len(words)
 for word in words:
     low = ""
     low = word.lower()
     low_words.append(low)
 
-
-for letter in string.printable:
+for letter in string.ascii_lowercase:
     letters[letter] = 0
-    
+
 for word in low_words:
     for c in word:
-        letters[c] += 1
+        if c in letters:
+            letters[c] += 1
 
-print(letters)
+#dict(sorted(letters.items(), key=lambda item: item[1])) sorta per valori
+letters = dict(sorted(letters.items(), key=lambda item: item[1], reverse=True))
+print(f"--- Begin report of {path_file} ---")
+print(f"{n_words} words found in the document\n")
+
+for k in letters:
+    print(f"The {k} character was found {letters[k]} times")
+
+print("--- End report ---")
+#You can use a string's .isalpha() method to check if a string only contains characters from the alphabet.
